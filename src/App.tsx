9e33ce8834dc98c7fda8c945ebcf7fe59a8982eb
Andrii Dotsenko <98803348+andriidotsenko@users.noreply.github.com>
2024-06-13 00:38:42 +0300

    test count

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { FC } from "react";
+import useContinuousCounter from "./hooks/useContinuousCounter";
 import "./App.css";
 
-function App() {
-  return <>test</>;
-}
+const App: FC = () => {
+  const { count, startIncrement, startDecrement, stopCounter } =
+    useContinuousCounter(0);
+
+  return (
+    <div className="root" style={{ userSelect: "none" }}>
+      <div className="count">Count: {count}</div>
+      <div className="increment">
+        <button
+          onMouseDown={startIncrement}
+          onMouseUp={stopCounter}
+          onMouseLeave={stopCounter}
+        >
+          Increment
+        </button>
+      </div>
+      <div className="decrement">
+        <button
+          onMouseDown={startDecrement}
+          onMouseUp={stopCounter}
+          onMouseLeave={stopCounter}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
